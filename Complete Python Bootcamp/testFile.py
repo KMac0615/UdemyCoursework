679702a8c4777062913c67c81c41b3d7a70079c3
Kelly McDonough <kmac0615@gmail.com>
2019-04-04 19:17:22 -0500

    first commit

---
+++ Complete Python Bootcamp/testFile.py
@@ -0,0 +1,4 @@
+print('hello world')
+
+# This is a comment
+print ('My name is Kelly.')
---
+++ Machine Learning A-Z/Datafiles/Machine Learning A-Z Template Folder/Part 1 - Data Preprocessing/data_preprocessing_template_km.py
@@ -0,0 +1,81 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul 29 10:47:00 2017
+
+@author: Kelly
+"""
+
+# Data Preprocessing
+
+# Importing the libraries
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Data.csv')
+
+# Create independent variable
+X = dataset.iloc[:, :-1].values
+
+# Create dependent variable
+y = dataset.iloc[: ,3].values
+
+
+# Taking care of missing data
+from sklearn.preprocessing import Imputer
+imputer = Imputer(missing_values = 'NaN', strategy = 'mean', axis = 0)
+imputer = imputer.fit(X[:, 1:3])
+X[:, 1:3] = imputer.transform(X[:, 1:3])
+
+# Encoding categorical data & Creating dummy variables
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelencoder_X = LabelEncoder()
+X[:, 0] = labelencoder_X.fit_transform(X[:, 0])
+onehotencoder = OneHotEncoder(categorical_features = [0])
+X = onehotencoder.fit_transform(X).toarray()
+labelencoder_y = LabelEncoder()
+y = labelencoder_X.fit_transform(y)
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
