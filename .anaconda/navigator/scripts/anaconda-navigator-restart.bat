54cb4d392fc5396d0a3ba1b53615cc4c136660d3
kmac1022 <kmac0615@gmail.com>
2019-10-10 15:14:33 -0500

    udemy

---
+++ .anaconda/navigator/scripts/anaconda-navigator-restart.bat
@@ -0,0 +1,3 @@
+chcp 1252
+call C:\Users\Kelly\Anaconda3\Scripts\activate C:\Users\Kelly\Anaconda3
+anaconda-navigator >C:\Users\Kelly\UdemyCoursework\.anaconda\navigator\scripts\anaconda-navigator-restart-out-1.txt 2>C:\Users\Kelly\UdemyCoursework\.anaconda\navigator\scripts\anaconda-navigator-restart-err-1.txt
---
+++ .anaconda/navigator/scripts/jupyterlab.bat
@@ -0,0 +1,3 @@
+chcp 1252
+call C:\Users\Kelly\Anaconda3\Scripts\activate C:\Users\Kelly\Anaconda3
+jupyter lab >C:\Users\Kelly\UdemyCoursework\.anaconda\navigator\scripts\jupyterlab-out-2.txt 2>C:\Users\Kelly\UdemyCoursework\.anaconda\navigator\scripts\jupyterlab-err-2.txt
