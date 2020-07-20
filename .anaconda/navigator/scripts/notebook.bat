e5696e8ec423b43a7c438650dac2e0ee2bbc9f40
kmac1022 <kmac0615@gmail.com>
2020-07-19 21:52:15 -0500

    remove ignored files

--- .anaconda/navigator/scripts/notebook.bat
+++
@@ -1,3 +0,0 @@
-chcp 1252
-call C:\Users\Kelly\Anaconda3\Scripts\activate C:\Users\Kelly\Anaconda3
-jupyter-notebook >C:\Users\Kelly\UdemyCoursework\.anaconda\navigator\scripts\notebook-out-4.txt 2>C:\Users\Kelly\UdemyCoursework\.anaconda\navigator\scripts\notebook-err-4.txt
